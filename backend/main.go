acfecc213fec832f1524eec2090c81e01c041187
wirat khamphan <wirat.kh@adv.local>
2023-12-13 16:10:03 +0700

    massage

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,34 @@ package main
 
 import (
 	condb "SHOP_LEK/database/condb"
+	"fmt"
 )
 
 func main() {
 	condb.NewEnv()
+	New_msg()
 
+}
+
+func New_msg() {
+	mag := "som message"
+
+	var msgPointer *string = &mag
+
+	fmt.Println(mag)
+	fmt.Println(*msgPointer)
+
+	changeMessage(&mag, "new message 1")
+	fmt.Println(mag)
+
+	changeMessage(msgPointer, "new message 2")
+	fmt.Println(mag)
+
+	changeMessage(msgPointer, "new message 3")
+	fmt.Println(*msgPointer)
+
+}
+
+func changeMessage(aPointer *string, newMessage string) {
+	*aPointer = newMessage
 }
