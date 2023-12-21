6813851a95d4414771bf9b8d5cf14bccec1f4f7a
Rlek <wirat0khamphan@gmail.com>
2023-12-21 13:10:29 +0700

    21-12-2566

--- backend/database/condb/condb.go
+++ backend/database/condb/condb.go
@@ -3,6 +3,7 @@ package condb
 import (
 	Env "SHOP_LEK/database/Env"
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -13,8 +14,17 @@ func NewConndb_mysql() (*sql.DB, error) {
 		return nil, err
 	}
 
-	db, err := sql.Open(env.DBDriver, env.DBUser+":"+env.DBPass+"@/"+env.DBName)
+	// db, err := sql.Open(env.DBDriver, env.DBUser+":"+env.DBPass+"@/"+env.DBName)
+	db, err := sql.Open(env.DBDriver, fmt.Sprintf("%s:%s@/%s", env.DBUser, env.DBPass, env.DBName))
+	if err != nil {
+		panic(err)
+	}
 
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	// defer db.Close()
 	return db, err
 
 }
